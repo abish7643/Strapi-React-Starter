885e14b6b9c2192e24451868a066e4b60994c958
Abish <abishvijayan7643@gmail.com>
2020-12-16 11:15:02 +0530

    Added _redirects and Removed HashRouter

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import {
   Switch,
   Route,
-  HashRouter as Router,
+  // HashRouter as Router,
   Redirect,
 } from "react-router-dom";
 
@@ -16,16 +16,16 @@ import NotFoundPage from "../NotFoundPage/index";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Nav />
-        <Switch>
-          <Route path="/" component={Articles} exact />
-          <Route path="/article/:slug" component={Article} exact />
-          <Route path="/category/:slug" component={Category} exact />
-          <Route path="/404" component={NotFoundPage} />
-          <Redirect to="404" />
-        </Switch>
-      </Router>
+      {/* <Router> */}
+      <Nav />
+      <Switch>
+        <Route path="/" component={Articles} exact />
+        <Route path="/article/:slug" component={Article} exact />
+        <Route path="/category/:slug" component={Category} exact />
+        <Route path="/404" component={NotFoundPage} />
+        <Redirect to="404" />
+      </Switch>
+      {/* </Router> */}
     </div>
   );
 }
